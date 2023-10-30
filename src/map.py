02b6c4d125d4a149f0680b4e2029d3150ab78b93
Akshay <sakshays.2000@gmail.com>
2023-10-30 21:04:19 +0100

    minor clean up

--- src/map.py
+++ src/map.py
@@ -3,7 +3,6 @@ import os
 
 import pygame
 from pygame.math import Vector2
-import random
 import assets
 import player
 from inventory import load_item
@@ -109,9 +108,7 @@ class Map:
                 x1 < self.item_locs[i][0] < x1 + rvp_size
                 and y1 < self.item_locs[i][1] < y1 + rvp_size
             ):
-                self.items[i].render(
-                    temp_surface, Vector2(self.item_locs[i]) - Vector2(rvp.topleft)
-                )
+                self.items[i].render(temp_surface, self.item_locs[i] - rvp.topleft)
         p.render(temp_surface, offset=-Vector2(rvp.topleft))
         self.update_lighting(p.get_tile(self.shader_scale), light_range, light_scale)
 
