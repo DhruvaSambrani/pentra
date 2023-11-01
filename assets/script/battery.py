8fbc499db3cb08ff76a42de20409d26d1c3b2824
Akshay <sakshays.2000@gmail.com>
2023-11-01 21:46:18 +0100

    remove item drop logic from battery script

--- assets/script/battery.py
+++ assets/script/battery.py
@@ -7,15 +7,16 @@ def main(app, player):
     if flashlight is None:
         clear_alerts(app)
         load_asset("script", "default.py", app=app, player=player)
+        return False
     else:
         if flashlight.item.state["charge"] > 0:
             clear_alerts(app)
             app.current_scenes.append(
                 load_asset("scene", "battery_no_use.scn", app=app)
             )
+            return False
         else:
             flashlight.item.state["charge"] = flashlight.item.state["max_charge"]
-            app.inventory.drop_item()
             clear_alerts(app)
             app.current_scenes.append(load_asset("scene", "battery_use.scn", app=app))
-    pass
+            return True
